66d7ea8397692fa0a65f085c9656f4a4c1ebe13f
ValentinD <valentindominguezruiz02@gmail.com>
2026-04-15 18:05:27 -0300

    Add files via upload

---
+++ Clase 6/E. 1.py
@@ -0,0 +1,6 @@
+numero_int = 10
+numero_float = 15.87
+palabra_string = "buenas noches"
+numero_bool = False
+
+print(f"int: {numero_int} float: {numero_float} string: {palabra_string} bool: {numero_bool}")
