ea05ea5dcdb6bcf2602dba199a7faa883692f12e
ValentinD <valentindominguezruiz02@gmail.com>
2026-04-15 18:54:02 -0300

    Add files via upload

---
+++ Clase 6/E. 4.py
@@ -0,0 +1,9 @@
+x = [10, -1, 2, 3, 5, 7, 6, -7, 8, -10]
+
+# for num in x:
+#    if num % 2 == 0:
+#        print(f"{num} es multiplo de dos")
+
+for i in range(len(x)):
+    if x[i] % 2 == 0:
+         print(f"{x[i]} es multiplo de dos") 
