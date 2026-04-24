979fc96cd39b0e287b0adf4b354977b420d7cdf2
ValentinD <valentindominguezruiz02@gmail.com>
2026-04-24 11:46:10 -0300

    Add files via upload

---
+++ Clase 7/E. 1.py
@@ -0,0 +1,14 @@
+# explica con tus palabras qué hace if, elif y else. ¿En qué caso usarías cada uno?
+
+estos operadores lo que hacen es permitirnos tomar decisiones en un codigo a partir de las 
+condiciones que queramos ponerles. Los usaría cuando necesite que el codigo tome un camino u otro
+dependiendo de diferentes condiciones.
+
+
+# explica con tus palabra qué hace while y en qué se diferencia de un for. ¿en qué caso usarías 
+# cada uno?
+
+el operador while lo que hace es generarno un bucle siempre y cuando se cumpla una condición, en 
+cambio un for lo que hace es que repetir algo una cierta cantidad de veces, pero no de manera infinita.
+Usaría un while cuando necesite que algo se repita una cantidad indefinida de veces, y un for cuando
+la cantidad de repeticiones tenga que ser exacta.
